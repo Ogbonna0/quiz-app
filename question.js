e9e25a136d1399b92ed43b551b3bd4e5234a2164
jacob <jacobjunior0105@gmail.com>
2022-09-06 15:40:51 +0100

    quiz-app

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question:" What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Processor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+   {
+        numb: 2,
+        question:" What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Stlyle Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question:" What does PHP Stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            " Hypertext Preprocessor",
+            " Hypertext Programming",
+            "Hypertext preprogramming",
+            "Hometext preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question:" What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylish Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question:" What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutible Markup Language",
+            "eXTra Multi-program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+];
